54ccb2635ebd9366a33f3dbd23bf7f80c4fa251c
ritan <38023004+rita-rita-ritan@users.noreply.github.com>
2021-06-28 10:53:07 +0900

    connec frontend proto to backend proto

---
+++ src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div id="app">
+    <h1>これはほーむがめんです</h1>
+    {{ info }}
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'App',
+
+  data () {
+    return {
+      info: null
+    }
+  },
+  mounted () {
+    axios
+      .get('/images')
+      .then(response => (this.info = response.data))
+  }
+}
+</script>
