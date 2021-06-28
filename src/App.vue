3afdfa23d7dd2736aa2548b2ea8aa989d11939ea
ritan <38023004+rita-rita-ritan@users.noreply.github.com>
2021-06-28 17:27:18 +0900

    introduce Vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,66 @@
 <template>
-  <div id="app">
-    <h1>これはほーむがめんです</h1>
-    {{ info }}
-  </div>
+  <v-app>
+    <v-app-bar
+      app
+      color="blue"
+      dark
+    >
+      <div class="d-flex align-center">
+        <!-- <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        /> -->
+        <h1>Pixiv Quiz</h1>
+
+        <!-- <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        /> -->
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <!-- <v-btn
+        href="https://github.com/vuetifyjs/vuetify/releases/latest"
+        target="_blank"
+        text
+      >
+        <span class="mr-2">Latest Release</span>
+        <v-icon>mdi-open-in-new</v-icon>
+      </v-btn> -->
+    </v-app-bar>
+
+    <v-main>
+      <div id="app">
+        <h2>
+          PixivQuiz is a game where you look at the works in the Pixiv ranking and guess the painter.
+        </h2>
+        <br>
+        <h3>API Result</h3>
+        {{ info }}
+      </div>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
+// import HelloWorld from './components/HelloWorld';
 import axios from 'axios'
 
 export default {
   name: 'App',
+
+  // components: {
+  //   HelloWorld,
+  // },
 
   data () {
     return {
@@ -20,6 +71,6 @@ export default {
     axios
       .get('/images')
       .then(response => (this.info = response.data))
-  }
-}
+  },
+};
 </script>
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,8 @@
 module.exports = {
-    devServer: {
-        proxy: 'http://localhost:8000'
-    }
+  transpileDependencies: [
+    'vuetify'
+  ],
+  devServer: {
+    proxy: 'http://localhost:8000'
+  }
 }
