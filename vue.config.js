d51732e431cd020bb039ca3d242f680ada4ab5e3
ritan <38023004+rita-rita-ritan@users.noreply.github.com>
2021-07-07 09:54:09 +0900

    update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,7 +1,12 @@
+const {API_BASE_URL} = process.env
+
 module.exports = {
   transpileDependencies: [
     'vuetify'
   ],
+  env: {
+    API_BASE_URL
+  },
   devServer: {
     proxy: API_BASE_URL
   }
